4d6df7de4e8f391beafa7dfa88a53c386baa9734
Rafiul Islam Jisan <rafiuljisan2005@gmail.com>
2025-12-14 22:53:39 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.hgbitkj.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+        app.get('/', (req, res) => {
+            res.send('server is running')
+        });
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+
+
+app.listen(port, () => {
+    console.log('server is running on port', port)
+})
